ff4e5daf08a37b6cf97ed0c128fb09190bc038b4
hhijry-coder <hhegri@ut.edu.sa>
2024-12-25 18:11:53 +0100

    Update googleapp.py

--- googleapp.py
+++ googleapp.py
@@ -142,30 +142,36 @@ class TabukEcoMoveOptimizer:
     def show_route_planner(self):
         st.subheader("Route Planner | مخطط الطريق")
         
-        if 'route_start' not in st.session_state:
-            st.session_state.route_start = list(CAMPUS_LOCATIONS.keys())[0]
-            st.session_state.route_end = list(CAMPUS_LOCATIONS.keys())[1]
-            st.session_state.show_route = False
-        
         col1, col2 = st.columns(2)
         with col1:
-            start = st.selectbox("Start Location | موقع البداية", 
-                               list(CAMPUS_LOCATIONS.keys()),
-                               key='route_start')
-            end = st.selectbox("End Location | موقع النهاية", 
-                             list(CAMPUS_LOCATIONS.keys()),
-                             key='route_end')
+            if 'start_location' not in st.session_state:
+                st.session_state.start_location = list(CAMPUS_LOCATIONS.keys())[0]
+            if 'end_location' not in st.session_state:
+                st.session_state.end_location = list(CAMPUS_LOCATIONS.keys())[1]
+            if 'show_route_map' not in st.session_state:
+                st.session_state.show_route_map = False
+                
+            selected_start = st.selectbox(
+                "Start Location | موقع البداية",
+                list(CAMPUS_LOCATIONS.keys()),
+                index=list(CAMPUS_LOCATIONS.keys()).index(st.session_state.start_location)
+            )
+            selected_end = st.selectbox(
+                "End Location | موقع النهاية",
+                list(CAMPUS_LOCATIONS.keys()),
+                index=list(CAMPUS_LOCATIONS.keys()).index(st.session_state.end_location)
+            )
             
             if st.button("Find Route | ابحث عن الطريق"):
-                st.session_state.show_route = True
-                st.session_state.route_start = start
-                st.session_state.route_end = end
+                st.session_state.start_location = selected_start
+                st.session_state.end_location = selected_end
+                st.session_state.show_route_map = True
         
         with col2:
             self.display_route_details()
             
-        if st.session_state.show_route:
-            self.calculate_route(st.session_state.route_start, st.session_state.route_end)
+        if st.session_state.show_route_map:
+            self.calculate_route(st.session_state.start_location, st.session_state.end_location)
 
     def calculate_route(self, start, end):
         if start and end and start != end:
